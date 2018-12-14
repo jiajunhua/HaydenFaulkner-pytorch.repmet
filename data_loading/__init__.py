b7fef2c2eb6f5b531677180b7bed18e7ff033ba2
HaydenFaulkner <hay167167>
2018-12-14 23:09:11 +1030

    added to init so can load the datasets without specifying their containing python file

---
+++ data_loading/__init__.py
@@ -0,0 +1,3 @@
+# This allows us to import the datasets without having to refer to their python file
+from data_loading.omniglot import OmniglotDataset
+from data_loading.oxford_flowers import OxfordFlowersDataset
