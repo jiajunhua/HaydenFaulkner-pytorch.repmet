44d6965327df65f8a5f95ee3dcb9f42d457eae8a
HaydenFaulkner <hay167167>
2018-12-17 18:50:40 +1030

    minor renames

--- losses/prototypical_loss.py
+++ losses/prototypical_loss.py
@@ -65,8 +65,8 @@ def prototypical_loss(input, target, n_support):
     target_inds = target_inds.view(n_classes, 1, 1)
     target_inds = target_inds.expand(n_classes, n_query, 1).long()
 
-    loss_val = -log_p_y.gather(2, target_inds).squeeze().view(-1).mean()
+    loss = -log_p_y.gather(2, target_inds).squeeze().view(-1).mean()
     _, y_hat = log_p_y.max(2)
-    acc_val = y_hat.eq(target_inds.squeeze()).float().mean()
+    acc = y_hat.eq(target_inds.squeeze()).float().mean()
 
-    return loss_val, acc_val
+    return loss, acc
