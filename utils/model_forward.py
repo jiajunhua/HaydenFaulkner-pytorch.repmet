c095c7abe5e1b1e844130af4e427b90f0bab166c
HaydenFaulkner <hayden.faulkner@adelaide.edu.au>
2018-12-21 11:24:14 +1030

    add a forward model pass funtion

---
+++ utils/model_forward.py
@@ -0,0 +1,26 @@
+
+from math import ceil
+
+import numpy as np
+
+import torch
+from torch.utils.data import DataLoader
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+def forward(model, dataset, chunk_size):
+    """Compute representations for input in chunks."""
+    chunks = int(ceil(float(len(dataset)) / chunk_size))
+    outputs = []
+    model.eval()
+    trainloader = DataLoader(dataset,
+                             batch_size=chunks,
+                             shuffle=False,
+                             num_workers=1)
+
+    for batch_idx, (inputs, labels) in enumerate(trainloader):
+        inputs = inputs.to(device)
+        output = model(inputs)
+        outs = output.data
+        outputs.append(outs.cpu().numpy())
+    return np.vstack(outputs)
