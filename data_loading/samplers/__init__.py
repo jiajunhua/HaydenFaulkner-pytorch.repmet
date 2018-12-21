08fbd1f0972d504952d08be5fef64bc455a0b77b
HaydenFaulkner <hayden.faulkner@adelaide.edu.au>
2018-12-21 11:27:20 +1030

    add magnet sampler and put samplers in a dir

---
+++ data_loading/samplers/__init__.py
@@ -0,0 +1,3 @@
+# This allows us to import the samplers without having to refer to their python file
+from data_loading.samplers.episode_batch import EpisodeBatchSampler
+from data_loading.samplers.magnet_batch import MagnetBatchSampler
