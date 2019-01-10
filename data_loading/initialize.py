a3ab33646f5260cab97ec07bdcbb96d671c18017
HaydenFaulkner <hay167167>
2019-01-10 23:04:02 +1030

    cleaned up detection set loading

--- data_loading/initialize.py
+++ data_loading/initialize.py
@@ -3,7 +3,6 @@ from torchvision.datasets import MNIST
 
 from data_loading.samplers import EpisodeBatchSampler, MagnetBatchSampler
 from data_loading.sets import OmniglotDataset, OxfordFlowersDataset, OxfordPetsDataset, StanfordDogsDataset, PascalVOCDataset, CombinedDataset
-from data_loading.utils import prepare_dataset
 from data_loading.detection_wrapper import DetectionWrapper
 
 def initialize_dataset(config, dataset_name, dataset_id, split, input_size, mean, std):
@@ -75,9 +74,7 @@ def initialize_dataset(config, dataset_name, dataset_id, split, input_size, mean
 
     elif dataset_name == 'voc':
         if dataset_id == '00':
-
-            transforms = trns.Compose([trns.ToTensor()])
-
+            transforms = None
             if split == 'train':
 
                 pv07 = PascalVOCDataset(root_dir=config.dataset.root_dir,
@@ -95,12 +92,17 @@ def initialize_dataset(config, dataset_name, dataset_id, split, input_size, mean
                                         use_flipped=config.dataset.use_flipped,
                                         use_difficult=config.dataset.use_difficult)
 
-                cd = CombinedDataset([pv07, pv12])
+                combined_set = CombinedDataset([pv07, pv12])
 
-                cd = prepare_dataset(cd)
-                cd = DetectionWrapper(cd, config, training=True, normalize=None)
+                detection_set = DetectionWrapper(dataset=combined_set,
+                                                 batch_size=config.train.batch_size,
+                                                 max_num_box=config.model.max_n_gt_boxes,
+                                                 scales=config.train.scales,
+                                                 max_size=config.train.max_size,
+                                                 use_all_gt=config.train.use_all_gt,
+                                                 training=True)
 
-                return cd
+                return detection_set
 
             elif split == 'val':
                 cd = PascalVOCDataset(root_dir=config.dataset.root_dir,
@@ -111,8 +113,6 @@ def initialize_dataset(config, dataset_name, dataset_id, split, input_size, mean
                                       use_flipped=config.dataset.use_flipped,
                                       use_difficult=config.dataset.use_difficult)
 
-                cd = prepare_dataset(cd)
-                cd = DetectionWrapper(cd, config, training=False, normalize=None)
 
                 return cd
 
@@ -175,3 +175,4 @@ def initialize_sampler(config, sampler_name, dataset, split):
             raise ValueError("Split '%s' not recognised for the %s sampler." % (split, sampler_name))
     else:
         raise ValueError("Sampler '%s' not recognised." % sampler_name)
+
