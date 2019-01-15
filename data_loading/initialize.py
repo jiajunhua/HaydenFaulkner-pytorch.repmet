83d4b962c28bae323f66852fd6079cba506f1aea
HaydenFaulkner <hay167167>
2019-01-15 13:49:47 +1030

    detection additions and modifications

--- data_loading/initialize.py
+++ data_loading/initialize.py
@@ -94,7 +94,7 @@ def initialize_dataset(config, dataset_name, dataset_id, split, input_size, mean
 
                 combined_set = CombinedDataset([pv07, pv12])
 
-                detection_set = DetectionWrapper(dataset=combined_set,
+                detection_set = DetectionWrapper(dataset=pv07,
                                                  batch_size=config.train.batch_size,
                                                  max_num_box=config.model.max_n_gt_boxes,
                                                  scales=config.train.scales,
@@ -105,16 +105,24 @@ def initialize_dataset(config, dataset_name, dataset_id, split, input_size, mean
                 return detection_set
 
             elif split == 'val':
-                cd = PascalVOCDataset(root_dir=config.dataset.root_dir,
-                                      split='test',
-                                      year='2007',
-                                      transform=transforms,
-                                      categories_subset=config.dataset.classes,
-                                      use_flipped=config.dataset.use_flipped,
-                                      use_difficult=config.dataset.use_difficult)
+                pv07 = PascalVOCDataset(root_dir=config.dataset.root_dir,
+                                        split='test',
+                                        year='2007',
+                                        transform=transforms,
+                                        categories_subset=config.dataset.classes,
+                                        use_flipped=config.dataset.use_flipped,
+                                        use_difficult=config.dataset.use_difficult)
 
 
-                return cd
+                detection_set = DetectionWrapper(dataset=pv07,
+                                                 batch_size=config.train.batch_size,
+                                                 max_num_box=config.model.max_n_gt_boxes,
+                                                 scales=config.train.scales,
+                                                 max_size=config.train.max_size,
+                                                 use_all_gt=config.train.use_all_gt,
+                                                 training=True)
+
+                return detection_set
 
             else:
                 raise ValueError("Split '%s' not recognised for the %s dataset (id: %s)." % (split, dataset_name, dataset_id))
@@ -178,9 +186,11 @@ def initialize_sampler(config, sampler_name, dataset, split):
             return DetectionSampler(n_samples=len(dataset),
                                     batch_size=config.train.batch_size)
         elif split == 'val':
-            return None
+            return DetectionSampler(n_samples=len(dataset),
+                                    batch_size=config.train.batch_size)
         elif split == 'test':
-            return None
+            return DetectionSampler(n_samples=len(dataset),
+                                    batch_size=config.train.batch_size)
         else:
             raise ValueError("Split '%s' not recognised for the %s sampler." % (split, sampler_name))
     else:
